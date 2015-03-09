9838fa61652551bcb3a96bd4c68f4952e2e779ea
bauli <lukaszdab4@wp.pl>
2015-03-09 23:01:58 +0100

    #10:Creating QuantityCheckingList and package structure .
    Step 2.

---
+++ src/main/java/cow/farm/utils/QuantityCheckingList.java
@@ -0,0 +1,16 @@
+package cow.farm.utils;
+
+import cow.farm.utils.LukaszGenericArrayList;
+
+import java.util.HashMap;
+
+public class QuantityCheckingList<T> extends LukaszGenericArrayList<T> {
+
+	public QuantityCheckingList() {
+	}
+
+	public HashMap<T , Integer> getQuantityOfEachObject(){
+
+	}
+
+}
