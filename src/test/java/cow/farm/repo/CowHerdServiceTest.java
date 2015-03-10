28e3f0c50f7c780fa2f3752097a14d33c924b84f
bauli <lukaszdab4@wp.pl>
2015-03-10 01:40:58 +0100

    #11:Add test for cow service.
    Test not finished, but finally started.

---
+++ src/test/java/cow/farm/repo/CowHerdServiceTest.java
@@ -0,0 +1,12 @@
+package cow.farm.repo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CowHerdServiceTest {
+
+	@Test
+	public schouldAddCowToListAndReturnHashMap(){
+		
+	}
+}
