8d02e436189ca3fa2a84799c2aef4fc8b5579053
bauli <lukaszdab4@wp.pl>
2015-03-10 01:35:51 +0100

    #11:Add makeCow implementation and Cow constructor.
    Doing progress in implementations of Cow Heard Service and Cow object

--- src/main/java/cow/farm/repo/Cow.java
+++ src/main/java/cow/farm/repo/Cow.java
@@ -3,13 +3,21 @@ package cow.farm.repo;
 
 public class Cow {
 
-String race;
+	String race;
 
 	public Cow() {
-		race=null;
+		race = null;
 	}
 
 	public Cow(String race) {
 		this.race = race;
+	}
+
+	public  String setRace(String race){
+		this.race = race;
+	}
+
+	public String getRace(){
+		return race;
 	}
 }
--- src/main/java/cow/farm/repo/CowHerdService.java
+++ src/main/java/cow/farm/repo/CowHerdService.java
@@ -1,6 +1,14 @@
 package cow.farm.repo;
 
 
+import cow.farm.utils.QuantityCheckingList;
+
 public class CowHerdService {
 
+	QuantityCheckingList<Cow> cowList = new QuantityCheckingList<>();
+
+	public makeCow(String race) {
+		Cow cow = new Cow();
+		cow.setRace(race);
+	}
 }
