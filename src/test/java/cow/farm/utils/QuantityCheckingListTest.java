aece3b847bc0afc5e5b0d6d76e570980dd77140d
bauli <lukaszdab4@wp.pl>
2015-03-12 00:16:31 +0100

    #10 Add test(not finished), correct in list.

--- src/test/java/cow/farm/utils/QuantityCheckingListTest.java
+++ src/test/java/cow/farm/utils/QuantityCheckingListTest.java
@@ -3,10 +3,23 @@ package cow.farm.utils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+
 public class QuantityCheckingListTest {
 
 	@Test
-	public schouldReturnMapOfValues(){
-		
+	public void shouldReturnMapOfValues(){
+		//given
+		QuantityCheckingList<String> list = new QuantityCheckingList();
+		//when
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		list.add("a");
+		list.add("b");
+		list.add("d");
+		//then
+		HashMap map = list.getQuantityOfEachObject();
+		Assert.assertEquals(0, );
 	}
 }
