e7af1caa4c73c4531988b50af39e1903250b0cb6
bauli <lukaszdab4@wp.pl>
2015-03-12 00:22:19 +0100

    #10 QuantityList Correct.
    Change loop parameter.

--- src/main/java/cow/farm/utils/QuantityCheckingList.java
+++ src/main/java/cow/farm/utils/QuantityCheckingList.java
@@ -7,19 +7,16 @@ import java.util.Map;
 
 public class QuantityCheckingList<T> extends LukaszGenericArrayList<T> {
 
-	public QuantityCheckingList() {
-	}
-
-	public HashMap<T , Integer> getQuantityOfEachObject(){
+	public HashMap<T, Integer> getQuantityOfEachObject() {
 		HashMap<T, Integer> hashMap = new HashMap<>();
-		int index=0;
-		for (T key:this){
-			if (!hashMap.containsValue(key)){
-				hashMap.put(key,1);
+		int value = 0;
+		for (T key : hashMap.keySet()) {
+			if (!hashMap.containsKey(key)) {
+				hashMap.put(key, 1);
 			}
-			index = hashMap.get(key);
-			index++;
-			hashMap.put(key,index);
+			value = hashMap.get(key);
+			value++;
+			hashMap.put(key, value);
 		}
 		return hashMap;
 	}
