b33d645cbb28f358d8c020b501aada2295611956
bauli <lukaszdab4@wp.pl>
2015-04-20 23:50:28 +0200

    #11 Cow Service.

--- src/main/java/cow/farm/repo/CowHerdService.java
+++ src/main/java/cow/farm/repo/CowHerdService.java
@@ -7,8 +7,12 @@ public class CowHerdService {
 
 	QuantityCheckingList<Cow> cowList = new QuantityCheckingList<>();
 
-	public makeCow(String race) {
+	public makeCow(Cow.CowRace race) {
 		Cow cow = new Cow();
 		cow.setRace(race);
+	}
+
+	public getHerdMap (){
+
 	}
 }
